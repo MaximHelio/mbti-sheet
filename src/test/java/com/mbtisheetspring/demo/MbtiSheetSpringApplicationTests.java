705843db3629973fc5c68f2e99b5cf79f4bc7503
kknni <gasiorowicz10@gmail.com>
2022-11-26 21:54:25 +0900

    mangrove pjt

---
+++ src/test/java/com/mbtisheetspring/demo/MbtiSheetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbtisheetspring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiSheetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
